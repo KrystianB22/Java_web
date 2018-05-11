a5fa0fe0a9e8af834493cec35b6ae6efedca838c
KrystianB22 <krystianv15@wp.pl>
2018-05-11 15:15:18 +0200

    Start

---
+++ src/main/java/com/web/first/web_first/WebFirstApplication.java
@@ -0,0 +1,12 @@
+package com.web.first.web_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFirstApplication.class, args);
+    }
+}
---
+++ src/test/java/com/web/first/web_first/WebFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.web.first.web_first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFirstApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
